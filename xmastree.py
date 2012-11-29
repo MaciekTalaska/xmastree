d4d0ff24faac73c8ba7626edef4df32f95a732a2
MaciekTalaska <maciek.talaska@gmail.com>
2012-11-29 02:15:37 +0100

    first version that stores programs

--- xmastree.py
+++ xmastree.py
@@ -5,10 +5,21 @@ import tornado.autoreload
 import threading
 import time
 import Queue
+import uuid
+import json
 
 lightThread = None
 queue = Queue.Queue()
 programs = dict()
+
+class LightChanger():
+    @staticmethod
+    def turn_all_ligths_off():
+        pass
+
+    @staticmethod
+    def change_light_state(light):        
+        pass
 
 class LightThread(threading.Thread):
     def __init__(self, threadId, name, counter):
@@ -17,38 +28,52 @@ class LightThread(threading.Thread):
     def run(self):
         pass
 
-class XmasLines(tornado.web.RequestHandler):
-    def get(self, line):
-        self.set_header("Content-Type", "application/json")
-        self.write("state for line " + str(line) + " requested")
+class JSONHelper():
+    def is_proper_program(self, program):
+        pass
     
-    def put(self, line):
+class RequestHandlerBase(tornado.web.RequestHandler):
+    def set_all_headers(self):
         self.set_header("Content-Type", "application/json")
-        self.write("line " +str(line) + " changed")
-        global queue
-        queue.put("a very new item in the queue", block=True)
+
+class XmasLines(RequestHandlerBase):
+    def put(self, line):
+        self.set_all_headers()
+        LightChanger.change_light_state(line)
 
 class XmasStdProgramLister(tornado.web.RequestHandler):
     def get(self):
         self.write("listing all standard programs...")
         
-class XmasStdProgram(tornado.web.RequestHandler):    
+class XmasStdProgram(RequestHandlerBase):    
     def get(self, program):
         self.write("standard program " +str(program) + " requested")
         
     def put(self):
         self.write("starting pre-defined program")
         
-class XmasCustomProgramLister(tornado.web.RequestHandler):
+class XmasCustomProgramLister(RequestHandlerBase):
     def get(self):
-        self.write("listing all CUSTOM programs...")
+        self.set_all_headers()
+        #self.write(str(programs.items()))
+        l = list()
+        for k in programs.iterkeys():
+            skey = str(k)
+            value = programs[k]
+            l.append(dict(skey, value))
+        output = json.dumps(l)
+        self.write(output)
 
-class XmasCustomProgram(tornado.web.RequestHandler):
-    def get(self):
-        self.write("custom program")
-        
-    def put(self):
-        self.write("uploading custom program")
+    def post(self):
+        id = uuid.uuid1()
+        self.write("{id:"+str(id)+"}")
+        global programs
+        programs[id]= self.request.body
+
+class XmasCustomProgram(RequestHandlerBase):
+    def get(self,id):  
+        newid = uuid.UUID(id)
+        self.write(programs[newid])
 
 application = tornado.web.Application([
     (r"/line/([0-7]{1})",XmasLines),
