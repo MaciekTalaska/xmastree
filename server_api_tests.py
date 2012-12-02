37355a84c60cdbac676c2bcc18d013cfec6648e0
MaciekTalaska <maciek.talaska@gmail.com>
2012-12-02 03:14:30 +0100

    code cleanup

--- server_api_tests.py
+++ server_api_tests.py
@@ -4,7 +4,7 @@ import uuid
 import json
 
 class TestBase(unittest.TestCase):
-    port = 8989
+    port = 8888
     def create_connection(self):
         return httplib.HTTPConnection("localhost", self.port)
         
@@ -22,26 +22,21 @@ class TestBase(unittest.TestCase):
         conn.request("POST", resource, body)
         response = conn.getresponse()
         status = response.status
-        body=response.read()
+        body = response.read()
         response.close()
         return (body,status)
 
 
 class TestStandardProgram(TestBase):
-    def test_there_should_be_2_standard_programms(self):
+    def test_there_should_be_standard_programms(self):
         response = self.execute_get_request("/stdprogram")
         status = response[1]
         self.assertEqual(200, status)
-        
     
     def test_asking_for_non_existing_program_should_result_in_404(self):
         sid = str(uuid.uuid4())
-        conn = self.create_connection()
-        conn.request("GET", "/stdprogram/"+sid)
-        response = conn.getresponse()
-        status = response.status
-        response.read()
-        response.close()
+        response = self.execute_get_request("/stdprogram/"+sid)
+        status = response[1]
         self.assertEqual(404, status)
     
 class TestCustomProgram(TestBase):
@@ -52,29 +47,23 @@ class TestCustomProgram(TestBase):
         self.assertEqual(404, status)
 
     def test_when_program_is_created_it_could_be_retrieved(self):
-        author="Maciek"
-        content="doesnt really matter"
-        name="a simple name for a test program"
+        author = "Maciek"
+        content = "doesnt really matter"
+        name = "a simple name for a test program"
         request_body = '{"author":"'+author+'","name":"'+name+'","content":"'+content+'"}'
-        response=self.execute_post_request("/program", request_body)
-        body=response[0]
-        status=response[1]
+        response = self.execute_post_request("/program", request_body)
+        body, status = response
         self.assertTrue(len(body)>0)
         self.assertEqual(200, status)
-        j=json.loads(body)
-        sid=j['id']
+        j = json.loads(body)
+        sid = j['id']
         response = self.execute_get_request("/program/"+sid)
-        status = response[1]
-        response_body=response[0]
+        response_body, status = response
         self.assertEqual(200, status)
         json_object = json.loads(response_body)
         self.assertEqual(author, json_object['author'])
         self.assertEqual(name, json_object['name'])
         self.assertEqual(content, json_object['content'])
 
-#suite = unittest.TestLoader().loadTestsFromTestCase(TestStandardProgram)
-#unittest.TextTestRunner(verbosity=2).run(suite)
-#suite = unittest.TestLoader().loadTestsFromTestCase(TestCustomProgram)
-#unittest.TextTestRunner(verbosity=2).run(suite)
 if __name__=='__main__':
     unittest.main()
