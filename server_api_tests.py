f65d95383f188e53c90e460ac2b4c84d8d0cd91b
MaciekTalaska <maciek.talaska@gmail.com>
2012-12-02 02:30:39 +0100

    changes to test. server port changed

--- server_api_tests.py
+++ server_api_tests.py
@@ -3,20 +3,40 @@ import httplib
 import uuid
 import json
 
-class TestStandardProgram(unittest.TestCase):
-    def test_there_should_be_2_standard_programms(self):
-        conn = httplib.HTTPConnection("localhost:8888")
-        conn.request("GET", "/stdprogram")
+class TestBase(unittest.TestCase):
+    port = 8989
+    def create_connection(self):
+        return httplib.HTTPConnection("localhost", self.port)
+        
+    def execute_get_request(self, resource):
+        conn = self.create_connection()
+        conn.request("GET", resource)
         response = conn.getresponse()
         status = response.status
-        response.read()
+        body = response.read()
         response.close()
+        return (body,status)
+        
+    def execute_post_request(self, resource, body):
+        conn = self.create_connection()
+        conn.request("POST", resource, body)
+        response = conn.getresponse()
+        status = response.status
+        body=response.read()
+        response.close()
+        return (body,status)
+
+
+class TestStandardProgram(TestBase):
+    def test_there_should_be_2_standard_programms(self):
+        response = self.execute_get_request("/stdprogram")
+        status = response[1]
         self.assertEqual(200, status)
         
     
     def test_asking_for_non_existing_program_should_result_in_404(self):
         sid = str(uuid.uuid4())
-        conn = httplib.HTTPConnection("localhost:8888")
+        conn = self.create_connection()
         conn.request("GET", "/stdprogram/"+sid)
         response = conn.getresponse()
         status = response.status
@@ -24,44 +44,33 @@ class TestStandardProgram(unittest.TestCase):
         response.close()
         self.assertEqual(404, status)
     
-class TestCustomProgram(unittest.TestCase):
+class TestCustomProgram(TestBase):
     def test_asking_for_non_existing_program_should_result_in_404(self):
         sid = str(uuid.uuid4())
-        conn = httplib.HTTPConnection("localhost:8888")
-        conn.request("GET", "/program/"+sid)
-        response = conn.getresponse()
-        status = response.status
-        response.read()
-        response.close()
+        response = self.execute_get_request("/program/"+sid)
+        status = response[1]
         self.assertEqual(404, status)
 
     def test_when_program_is_created_it_could_be_retrieved(self):
         author="Maciek"
         content="doesnt really matter"
         name="a simple name for a test program"
-        conn = httplib.HTTPConnection("localhost:8888")
-        conn.request("POST", "/program",'{"author":"'+author+'","name":"'+name+'","content":"'+content+'"}')
-        response = conn.getresponse()
-        status = response.status
-        body=response.read()
-        response.close()
+        request_body = '{"author":"'+author+'","name":"'+name+'","content":"'+content+'"}'
+        response=self.execute_post_request("/program", request_body)
+        body=response[0]
+        status=response[1]
         self.assertTrue(len(body)>0)
         self.assertEqual(200, status)
         j=json.loads(body)
         sid=j['id']
-        conn = httplib.HTTPConnection("localhost:8888")
-        conn.request("GET", "/program/"+sid)
-        response = conn.getresponse()
-        status = response.status
-        response_body=response.read()
-        response.close()
+        response = self.execute_get_request("/program/"+sid)
+        status = response[1]
+        response_body=response[0]
         self.assertEqual(200, status)
         json_object = json.loads(response_body)
         self.assertEqual(author, json_object['author'])
         self.assertEqual(name, json_object['name'])
         self.assertEqual(content, json_object['content'])
-        
-        
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestStandardProgram)
 unittest.TextTestRunner(verbosity=2).run(suite)
